07ab2b87ba48135c7310e1c3e1e2318226034487
Kai Harder <kai.harder@sap.com>
2026-06-10 10:16:57 +0000

    Ignore isinstance calls in FTP240

--- tests/checks/test_ast_name_or_attribute.py
+++ tests/checks/test_ast_name_or_attribute.py
@@ -464,14 +464,14 @@ class TestFTP240:
 
         if attr_name in MOCK_DECORATOR_ARG_ALLOWED:
             assert results == [
-                FTP240(line=18, column=1, attr=attr_name),
-                FTP240(line=21, column=5, attr=attr_name),
-                FTP240(line=25, column=9, attr=attr_name),
+                FTP240(line=19, column=1, attr=attr_name),
+                FTP240(line=22, column=5, attr=attr_name),
+                FTP240(line=26, column=9, attr=attr_name),
             ]
         else:
             assert results == [
-                FTP240(line=18, column=1, attr=attr_name),
-                FTP240(line=21, column=5, attr=attr_name),
-                FTP240(line=25, column=9, attr=attr_name),
-                FTP240(line=27, column=2, attr=attr_name),
+                FTP240(line=19, column=1, attr=attr_name),
+                FTP240(line=22, column=5, attr=attr_name),
+                FTP240(line=26, column=9, attr=attr_name),
+                FTP240(line=28, column=2, attr=attr_name),
             ]
